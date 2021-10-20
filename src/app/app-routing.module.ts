c5a6ab311f743f330961c5ec3d93a18e0755d98a
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-10-19 22:27:42 -0300

    Outlined the login and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PostsComponent } from './pages/posts/posts.component';
+// import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent
+  // },
   {
-    path: '',
-    component: HomeComponent
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'albums',
+    component: AlbumsComponent
+  },
+  {
+    path: 'todos',
+    component: TodosComponent
   },
   {
     path: '**',
     pathMatch: 'full',
-    redirectTo: ''
+    redirectTo: 'login'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UsersComponent } from './pages/users/users.component';
 import { PostsComponent } from './pages/posts/posts.component';
 import { AlbumsComponent } from './pages/albums/albums.component';
 import { TodosComponent } from './pages/todos/todos.component';
+import { MaterialModule } from './shared/modules/material/material.module';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { TodosComponent } from './pages/todos/todos.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
